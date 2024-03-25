d5e820aac8c22384881418feb07896a7933db708
Evan Galli <evan_g@orange.fr>
2024-03-25 22:43:30 +0100

    feat: Delivery activity

--- src/app/src/main/java/edu/ihm/td1/livraison/DeliveryActivity.java
+++ src/app/src/main/java/edu/ihm/td1/livraison/DeliveryActivity.java
@@ -1,16 +1,27 @@
 package edu.ihm.td1.livraison;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class DeliveryActivity extends AppCompatActivity {
+
+    private ArrayList<Delivery> deliveries = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_delivery);
+
+        deliveries.add(new Delivery("435 Rue du Chemin, Valbonne 06560"));
+        deliveries.add(new Delivery("25 avenue de l’église, Biot 06510"));
+        deliveries.add(new Delivery("1 bis rue de l’étoile, Antibes 06600"));
+
+        DeliveryAdapter deliveryAdapter = new DeliveryAdapter(getApplicationContext(), deliveries);
+        ListView nextDeliveries = findViewById(R.id.next_delivery);
+        nextDeliveries.setAdapter(deliveryAdapter);
     }
 }
