90a1c1da956fb2d780eb1e70c867290dfe752072
Gwendolyne Bouchard <gwendolyne.bouchard@etu.univ-cotedazur.fr>
2024-04-02 17:27:23 +0200

    profile

--- src/app/src/main/java/edu/ihm/td1/livraison/Toolbar.java
+++ src/app/src/main/java/edu/ihm/td1/livraison/Toolbar.java
@@ -7,17 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class Toolbar extends Fragment {
-    private ImageView profile;
+    private TextView profile;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.toolbar, container, false);
 
-        profile = getView().findViewById(R.id.icone_client);
-        profile.setOnClickListener(view -> startActivity(new Intent(getContext(), DeliveryActivity.class)));
+        profile =  rootView.findViewById(R.id.client_toolbar);
+        profile.setOnClickListener(view -> startActivity(new Intent(getContext(), ProfileActivity.class)));
 
         return rootView;
     }
