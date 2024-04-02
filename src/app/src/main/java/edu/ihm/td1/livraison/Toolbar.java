0f4721b409d224a0fb377d399e884a136c9a9b93
Gwendolyne Bouchard <gwendolyne.bouchard@etu.univ-cotedazur.fr>
2024-04-02 15:53:41 +0200

    debut profile

---
+++ src/app/src/main/java/edu/ihm/td1/livraison/Toolbar.java
@@ -0,0 +1,18 @@
+package edu.ihm.td1.livraison;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ToolbarFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_toolbar, container, false);
+        //do something
+        return rootView;
+    }
+}
