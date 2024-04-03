390a9326db91e99bcc73c22bb6a8cd07c9d34011
AmelieMuller <amelie17.muller@gmail.com>
2024-04-03 18:49:28 +0200

    Camera : 1er tuto de Rallo suivit

--- src/app/src/main/java/edu/ihm/td1/livraison/PictureFragment.java
+++ src/app/src/main/java/edu/ihm/td1/livraison/PictureFragment.java
@@ -1,2 +1,55 @@
-package edu.ihm.td1.livraison;public class PictureFragment {
+package edu.ihm.td1.livraison;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import android.Manifest;
+
+import java.util.Objects;
+
+public class PictureFragment extends Fragment {
+    public PictureFragment(){}
+    private ImageView imageView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate( R.layout.fragment_picture, container, false);
+
+        imageView = rootView.findViewById(R.id.phototaken);
+        rootView.findViewById( R.id.buttonImage).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(ContextCompat.checkSelfPermission( getContext(), Manifest.permission.CAMERA)== PackageManager.PERMISSION_DENIED){
+                    ActivityCompat.requestPermissions( getActivity(), new String[] {Manifest.permission.CAMERA}, IPictureActivity.REQUEST_CAMERA );
+                }
+                else{
+                    takePicture();
+                }
+            }
+        });
+
+        return rootView;
+
+    }
+
+    public void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        getActivity().startActivityForResult( intent, IPictureActivity.REQUEST_CAMERA);
+    }
+
+    public void setImage(Bitmap bitmap){
+        imageView.setImageBitmap(bitmap);
+    }
+
 }
--- src/app/src/main/java/edu/ihm/td1/livraison/ReportCommandActivity.java
+++ src/app/src/main/java/edu/ihm/td1/livraison/ReportCommandActivity.java
@@ -1,22 +1,75 @@
 package edu.ihm.td1.livraison;
 
+import static android.widget.Toast.makeText;
+
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-public class ReportCommandActivity extends AppCompatActivity {
+public class ReportCommandActivity extends AppCompatActivity implements IPictureActivity{
 
+    private Bitmap picture;
+    private PictureFragment picturefragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_report_command);
+        picturefragment = (PictureFragment) getSupportFragmentManager().findFragmentById( R.id.fragmentPicture) ;
+        if(picturefragment == null){
+            picturefragment = new PictureFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace( R.id.phototaken, picturefragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast toast = makeText(getApplicationContext(), " CAMERA authorisation granted", Toast.LENGTH_LONG);
+                    toast.show();
+                    picturefragment.takePicture();
+                } else {
+                    Toast toast = makeText(getApplicationContext(), " CAMERA authorisation NOT granted", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult( requestCode , resultCode , data);
+        if(requestCode == REQUEST_CAMERA){
+            if (resultCode == RESULT_OK) {
+                picture = (Bitmap) data.getExtras().get("data");
+                picturefragment.setImage(picture);              // .setImage(picture selon Rallo
+            }
+            else if(resultCode ==RESULT_CANCELED){
+                Toast toast = Toast.makeText(getApplicationContext(), "Picrture canceled", Toast.LENGTH_LONG);
+                toast.show();
+            }
+            else{
+                Toast toast = Toast.makeText(getApplicationContext(), "Action failed", Toast.LENGTH_LONG);
+                toast.show();
+            }
+
+        }
 
     }
 }
