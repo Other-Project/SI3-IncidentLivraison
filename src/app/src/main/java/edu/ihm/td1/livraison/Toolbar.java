fea727fdc59db2c5024f5077204e859ef46fe9e8
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-05-24 09:28:35 +0200

    feat : Added a logout button in toolbar

--- src/app/src/main/java/edu/ihm/td1/livraison/Toolbar.java
+++ src/app/src/main/java/edu/ihm/td1/livraison/Toolbar.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,35 +19,42 @@ import java.util.concurrent.TimeUnit;
 import edu.ihm.td1.livraison.userFactory.User;
 
 public class Toolbar extends Fragment {
-    private TextView profile;
     private String nomPrenom;
     private User user;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.toolbar, container, false);
-        profile =  rootView.findViewById(R.id.client_toolbar);
-
-        profile.setOnClickListener(view ->{
-            Intent intent = new Intent(getContext(), ProfileActivity.class);
-            intent.putExtra("User",user);
+        ImageView logout = rootView.findViewById(R.id.logout);
+        logout.setOnClickListener(view -> {
+            Intent intent = new Intent(getContext(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
         });
 
-
+        TextView profile = rootView.findViewById(R.id.client_toolbar);
+        profile.setOnClickListener(view -> {
+            Intent intent = new Intent(getContext(), ProfileActivity.class);
+            intent.putExtra("User", user);
+            startActivity(intent);
+        });
         profile.setText(nomPrenom);
+
+
 
         return rootView;
     }
 
     public void notifyClientIsReady(TextView textView) {
         Bundle bundle = getArguments();
-        if(bundle != null){
+        if (bundle != null) {
             user = bundle.getParcelable("User");
-            nomPrenom = user.getPrenom()+" "+user.getNom();
+            nomPrenom = user.getPrenom() + " " + user.getNom();
         }
     }
 }
