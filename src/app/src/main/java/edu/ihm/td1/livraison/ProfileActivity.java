99c4c871a9ff5359aecbb78b8934834b59763a60
Evan Galli <evan_g@orange.fr>
2024-05-28 23:11:56 +0200

    refactor: Suppress warnings in ProfileActivity

--- src/app/src/main/java/edu/ihm/td1/livraison/ProfileActivity.java
+++ src/app/src/main/java/edu/ihm/td1/livraison/ProfileActivity.java
@@ -2,9 +2,11 @@ package edu.ihm.td1.livraison;
 
 import static edu.ihm.td1.livraison.Notification.CHANNEL_1_ID;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -14,40 +16,43 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import java.util.Locale;
+
 import edu.ihm.td1.livraison.userFactory.Client;
 import edu.ihm.td1.livraison.userFactory.Livreur;
 import edu.ihm.td1.livraison.userFactory.Sav;
 import edu.ihm.td1.livraison.userFactory.User;
 
 public class ProfileActivity extends AppCompatActivity {
-    private User user;
 
     private void sendNotificationOnChannel(String title, String content, String channelId, int priority) {
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.icone_client);
-        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, channelId)
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.icone_client);
+        android.app.Notification notification = new NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.drawable.check)
                 .setContentTitle(title)
                 .setContentText(content)
                 .setPriority(priority)
                 .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap))
-                .setTimeoutAfter(5000);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 1);
-            return;
-        }
-        NotificationManagerCompat.from(this).notify(0, notification.build());
+                .setTimeoutAfter(5000)
+                .build();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+        else NotificationManagerCompat.from(this).notify(0, notification);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        user = getIntent().getParcelableExtra("User");
+        User user = getIntent().getParcelableExtra("User");
+        assert user != null;
 
         setContentView(R.layout.activity_profile);
-        findViewById( R.id.button ).setOnClickListener( click -> {
+        findViewById(R.id.button).setOnClickListener(click -> {
             String title = "Livraisoon";
             String message = "Modifications enregristrées";
-            sendNotificationOnChannel( title, message, CHANNEL_1_ID, NotificationCompat.PRIORITY_HIGH );
+            sendNotificationOnChannel(title, message, CHANNEL_1_ID, NotificationCompat.PRIORITY_HIGH);
         });
 
         TextView prenom = findViewById(R.id.prenom_edit);
@@ -56,16 +61,16 @@ public class ProfileActivity extends AppCompatActivity {
         prenom.setText(user.getPrenom());
         TextView nom = findViewById(R.id.nom_edit);
         nom.setText(user.getNom());
-        if(user instanceof Sav){
-            findViewById(R.id.adress).setVisibility(View.INVISIBLE);
-            findViewById(R.id.num_tel).setVisibility(View.INVISIBLE);
-        }else if(user instanceof Livreur){
+        if (user instanceof Sav) {
+            findViewById(R.id.adress).setVisibility(View.GONE);
+            findViewById(R.id.num_tel).setVisibility(View.GONE);
+        } else if (user instanceof Livreur) {
             Livreur livreur = (Livreur) user;
-            numTel.setText("0"+livreur.getNumTelephone());
-            findViewById(R.id.adress).setVisibility(View.INVISIBLE);
-        }else{
+            numTel.setText(String.format(Locale.FRANCE, "%010d", livreur.getNumTelephone()));
+            findViewById(R.id.adress).setVisibility(View.GONE);
+        } else {
             Client client = (Client) user;
-            numTel.setText("0"+client.getNumTelephone());
+            numTel.setText(String.format(Locale.FRANCE, "%010d", client.getNumTelephone()));
             adresse.setText(client.getAdresse().toString());
         }
     }
