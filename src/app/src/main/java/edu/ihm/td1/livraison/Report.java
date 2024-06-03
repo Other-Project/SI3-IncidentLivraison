fc37a3c04f0b6b9bf7a1617a66e72608b7a0d0fd
AmelieMuller <amelie17.muller@gmail.com>
2024-06-03 13:17:07 +0200

    mode tablette

--- src/app/src/main/java/edu/ihm/td1/livraison/Report.java
+++ src/app/src/main/java/edu/ihm/td1/livraison/Report.java
@@ -11,22 +11,22 @@ import java.util.List;
 
 public class Report implements Parcelable {
     public static List<Report> REPORTS = new ArrayList<>(List.of(
-            new Report(Order.ORDERS.get(0),"Trop petit","",false ,1),
-            new Report(Order.ORDERS.get(1),"Il ne parle pas, déçu de la marchandise","",false ,2),
-            new Report(Order.ORDERS.get(2),"Pas assez rose","",false ,3),
-            new Report(Order.ORDERS.get(3),"La fermeture ne marche pas","",false ,4),
-            new Report(Order.ORDERS.get(4), "Il fait 2cm de haut", "", false, 5),
-            new Report(Order.ORDERS.get(5),"Il est troué","",true,6),
-            new Report(Order.ORDERS.get(6), "Il ne chante pas 'Sac-à-dos Sac-à-dos'", "", true, 7),
-            new Report(Order.ORDERS.get(7),"Une bretelle est déchirée","",true,8),
-            new Report(Order.ORDERS.get(8),"Il manque la carte","",true,9)
+            new Report(Order.ORDERS.get(0),"Trop petit",R.drawable.sac_report6,false ,1),
+            new Report(Order.ORDERS.get(1),"Il ne parle pas, déçu de la marchandise",R.drawable.sac_report1,false ,2),
+            new Report(Order.ORDERS.get(2),"Pas assez rose",R.drawable.sac_report2,false ,3),
+            new Report(Order.ORDERS.get(3),"La fermeture ne marche pas",R.drawable.sac_report3,false ,4),
+            new Report(Order.ORDERS.get(4), "Il fait 2cm de haut", R.drawable.sac_report4, false, 5),
+            new Report(Order.ORDERS.get(5),"Il est troué",R.drawable.sac_report5,true,6),
+            new Report(Order.ORDERS.get(6), "Il ne chante pas 'Sac-à-dos Sac-à-dos'", R.drawable.sac_report6, true, 7),
+            new Report(Order.ORDERS.get(7),"Une bretelle est déchirée",R.drawable.sac_report1,true,8),
+            new Report(Order.ORDERS.get(8),"Il manque la carte",R.drawable.sac_report2,true,9)
     ));
     Order order;
     String descriptionProbleme;
-    String imgProblem;
+    int imgProblem;
     Boolean isTreated;
     int id ;
-    public Report(Order order, String desc,String img, Boolean treadted, int id){
+    public Report(Order order, String desc,int img, Boolean treadted, int id){
         this.order = order;
         this.descriptionProbleme = desc;
         this.imgProblem = img;
@@ -37,7 +37,7 @@ public class Report implements Parcelable {
     protected Report(Parcel in) {
         order = in.readParcelable(Order.class.getClassLoader());
         descriptionProbleme = in.readString();
-        imgProblem = in.readString();
+        imgProblem = in.readInt();
         id = in.readInt();
     }
 
@@ -45,7 +45,7 @@ public class Report implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeParcelable(order, flags);
         dest.writeString(descriptionProbleme);
-        dest.writeString(imgProblem);
+        dest.writeInt(imgProblem);
         dest.writeInt(id);
     }
 
@@ -84,7 +84,7 @@ public class Report implements Parcelable {
         return order;
     }
 
-    public String getImgProblem(){
+    public int getImgProblem(){
         return imgProblem;
     }
 
--- src/app/src/main/java/edu/ihm/td1/livraison/TabletActivity.java
+++ src/app/src/main/java/edu/ihm/td1/livraison/TabletActivity.java
@@ -5,6 +5,7 @@ import static android.content.ContentValues.TAG;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.os.Vibrator;
@@ -28,7 +29,7 @@ public class TabletActivity extends AppCompatActivity {
     public static Report report;
     public static ObjectDisplayFragment objectDisplayFragment;
     public static TextView textView;
-    public final static Report NO_REPORT = new Report( new Order(0, "Pas de commandes à traiter", 0 ,true,"",0), "Pas de commandes à traiter", "", false,0);
+    public final static Report NO_REPORT = new Report( new Order(0, "Pas de commandes à traiter", 0 ,true,"",0), "Pas de commandes à traiter", R.drawable.logo, false,0);
     public static void setReport(Report r) {
         report = r;
     }
@@ -83,6 +84,11 @@ public class TabletActivity extends AppCompatActivity {
         Bundle bundle1 = new Bundle();
         bundle1.putParcelable(ObjectDisplayFragment.ARG_ITEM, report);
         objectFrag.setArguments(bundle1);
+        TextView text = findViewById(R.id.text_signalement);
+        setTextView(text);
+        text.setText(report.descriptionProbleme);
+        ImageView image = findViewById(R.id.image_signalement);
+        image.setImageResource(report.getImgProblem());
         //Deuxième activité :
         //this.refreshReport();
 
@@ -113,8 +119,7 @@ public class TabletActivity extends AppCompatActivity {
         }
         ObjectDisplayFragment objectFrag = (ObjectDisplayFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentObjectDisplay);
         setObjectDisplayFragment(objectFrag);
-        Log.d("Report", (report==null)? "null" : "not null");/*
-        */
+        Log.d("Report", (report==null)? "null" : "not null");
         objectFrag.setItem(report);
         objectFrag.changeDisplayedObject();
 
@@ -122,6 +127,7 @@ public class TabletActivity extends AppCompatActivity {
         setTextView(text);
         text.setText(report.descriptionProbleme);
         ImageView image = findViewById(R.id.image_signalement);
+        image.setImageResource(report.getImgProblem());
 
     }
 
